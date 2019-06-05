a063c4a9bb57360616b65674c8f248eb9b2abf0a
GianluigiMemoli <g.memoli98@gmail.com>
2019-06-05 11:12:56 +0200

    Alpha release. Everything since opening an SSH connection works, but the printer prints blank pages.

---
+++ print.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+HOSTNAME="gserver"
+SERVERADDR="192.168.178.51"
+DIRNAME="~/print"
+
+#for each file in dir
+for file in * 
+do
+    #Check if the current filename is not this script 
+    if [ "$file" != "$0" ]
+    then   
+        #Move each file to the server then delete from this DIR     
+        scp "$file" $HOSTNAME@$SERVERADDR:$DIRNAME
+        #rm "$file"
+    fi
+done 
+
+#Access to the server through SSH then print sent files
+ssh -tt $HOSTNAME@$SERVERADDR << ENDREMOTESCRIPT
+    cd print 
+    for file in *
+    do
+        lp "$file"
+    done  
+    rm *
+    logout 
+ENDREMOTESCRIPT
+
+echo "DONE!"
+
